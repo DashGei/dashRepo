9381b13d76e6ea52264b9ddf848ef806aacd7aac
DashGei <dashgeiser@gmail.com>
2026-04-13 18:55:30 -0400

    js working accumulate and spend

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,24 @@
-const button = document.querySelector("button")
-const counter = document.getElementById("counter")
-button.addEventListener("click", () => counter.innerHTML++)
-localStorage.setitem("counter", counter.innerHTML);
+const counter = document.getElementById("counter");
+const addButton = document.getElementById("addButton");
+const resetButton = document.getElementById("resetButton");
+
+let saved = localStorage.getItem("counterValue");
+counter.innerHTML = saved ? saved : "0";
+
+addButton.addEventListener("click", () => {
+    let current = Number(counter.innerHTML);
+    current++;
+    counter.innerHTML = current;
+    
+    localStorage.setItem("counterValue", current.toString());
+});
+
+resetButton.addEventListener("click", () => {
+    let zero = 0;
+    
+    counter.innerHTML = 0;
+    const audio = new Audio('images/register.mp3');
+    audio.play();
+    localStorage.setItem("counterValue", zero.toString());
+});
+
